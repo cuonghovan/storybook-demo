724f78382507999d52fac1fe1d400b5561cfac38
Cuong Ho <cuonghv21@gmail.com>
2024-12-27 20:08:37 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h4">Welcome to My MUI App</Typography>
+    </div>
+  );
+}
+
+export default App;
